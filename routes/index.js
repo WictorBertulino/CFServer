cf88e846bf4002e4458bbc13f55032212e421a24
wictorbertulino <wictorbertulino@gmail.com>
2020-03-13 14:19:49 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -15,24 +15,18 @@ router.post('/GetUser', function (req, res, next) {
   res.setHeader("Content-Type", "application/json");
   con.connect(function(err) {
     if (err) throw err;
-    con.query("SELECT * FROM usuarios WHERE gamertag = '"+req.body.gamertag +"' AND senha='"+req.body.password+"';", function (err, result, fields) {
-      if (err) throw err;
-      console.log(result);
-      res.send({teste:1})
-      con.end(function(err) {
-        if (err) {
-          return console.log('error:' + err.message);
-        }
-        console.log('Close the database connection.');
-      });
-
-      // res.send(JSON.stringify(result));
-    });
-
   });
 
-  
- 
+  con.query("SELECT * FROM usuarios WHERE gamertag = '"+req.body.gamertag +"' AND senha='"+req.body.password+"';", function (err, result, fields) {
+    if (err) throw err;
+    res.send(JSON.stringify(result));
+  });
+  con.end(function(err) {
+    if (err) {
+      return console.log('error:' + err.message);
+    }
+    console.log('Close the database connection.');
+  });
 });
 
 module.exports = router;
