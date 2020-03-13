82257a3c0b4d4f532434d06ac0bb696e6dc59e40
wictorbertulino <wictorbertulino@gmail.com>
2020-03-13 14:29:24 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,17 +2,18 @@ var express = require('express');
 var router = express.Router();
 var mysql = require('mysql');
 
-var con = mysql.createConnection({
-  host: "jcrastreamentoveicular00.com.br",
-  user: "jcdev",
-  password: "jc123",
-  database: "cf"
-});
 
 
 /* GET home page. */
 router.post('/GetUser', function (req, res, next) {
   res.setHeader("Content-Type", "application/json");
+  var con = mysql.createConnection({
+    host: "jcrastreamentoveicular00.com.br",
+    user: "jcdev",
+    password: "jc123",
+    database: "cf"
+  });
+  
   con.connect();
   con.query("SELECT * FROM usuarios WHERE gamertag = '"+req.body.gamertag +"' AND senha='"+req.body.password+"';",function (err, result, fields) {
       if (err) throw err;
