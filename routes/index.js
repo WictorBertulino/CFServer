301f7d6954779d9ea3344542dcf9e70807af0618
wictorbertulino <wictorbertulino@gmail.com>
2020-03-13 14:25:10 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -13,21 +13,21 @@ var con = mysql.createConnection({
 /* GET home page. */
 router.post('/GetUser', function (req, res, next) {
   res.setHeader("Content-Type", "application/json");
-  con.connect(function(err) {
-    if (err) throw err;
-  });
-
-  con.query("SELECT * FROM usuarios WHERE gamertag = '"+req.body.gamertag +"' AND senha='"+req.body.password+"';", function (err, result, fields) {
-    if (err) throw err;
-    con.end(function(err) {
-      if (err) {
-        return console.log('error:' + err.message);
-      }
-      console.log('Close the database connection.');
-    });
-    res.send(JSON.stringify(result));
-  });
-
+  con.connect();
+  con.query("SELECT * FROM usuarios WHERE gamertag = '"+req.body.gamertag +"' AND senha='"+req.body.password+"';",function (err, result, fields) {
+      if (err) throw err;
+      console.log(result);
+      
+      res.send(JSON.stringify(result));
+    })
+ // con.query("SELECT * FROM usuarios WHERE gamertag = '"+req.body.gamertag +"' AND senha='"+req.body.password+"';", function (err, result, fields) {
+    //   if (err) throw err;
+    //   console.log(result);
+      
+    //   // res.send(JSON.stringify(result));
+    // });
+  
+ 
 });
 
 module.exports = router;
