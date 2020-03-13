edf82fed7a4f420301c41579632077af9736a1c3
wictorbertulino <wictorbertulino@gmail.com>
2020-03-13 10:55:11 -0400

    first commit

--- routes/index.js
+++ routes/index.js
@@ -2,14 +2,10 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/GetUser', function(req, res, next) {
+router.post('/GetUser', function(req, res, next) {
 
-    let obj =
-    { 
-      Gamertag:"bertz Cf",
-      senha:"123",
-    }
-    res.end(JSON.stringify(obj));
+   
+    res.end(JSON.stringify(req));
 });
 
 module.exports = router;
